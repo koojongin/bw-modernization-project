1b8777ce5e15abede95ad1b62245742b5a605dd7
jonginkoo <jiku90@ddingdong.net>
2022-08-04 09:26:16 +0900

    feat: half done

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const a = 'test';
-console.log(a);
+import fs from 'fs';
+import concat from 'concat-stream';
+import createExtractor from 'scm-extractor';
+
+fs.createReadStream('./test.scx')
+  .pipe(createExtractor())
+  .pipe(
+    concat((data) => {
+      // do something with the CHK data here...
+      const scenarioChk = data;
+      console.log(scenarioChk);
+    }),
+  );
